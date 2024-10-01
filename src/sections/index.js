cafca9a17ef253d5e6fa2d87101664acdb571adf
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-01 12:14:24 +0530

    Make components  for sections

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+import SpecialOffer from "./SpecialOffer";
+import Subscribes from "./Subscribes";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  CustomerReviews,
+  Footer,
+  SpecialOffer,
+  Subscribes,
+};
