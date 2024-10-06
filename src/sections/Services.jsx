1829523b90362331803246df2144701ab2a66604
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-06 21:45:10 +0530

    Implement Services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import ServicesCard from "../components/ServicesCard";
+import { services } from "../constants";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((services) => (
+        <ServicesCard key={services.label} {...services} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
