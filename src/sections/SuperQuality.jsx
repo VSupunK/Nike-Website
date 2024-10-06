baa158f39dd208c7b207d2fbbb4703a9d2dd4c8b
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-06 20:44:40 +0530

    Implement SuperQuality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,50 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            We Provide You
+          </span>
+          <span className="text-coral-red inline-block mt-3">Super&nbsp;</span>
+          <span className="text-coral-red inline-block mt-3">
+            Quality&nbsp;
+          </span>
+          Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to details and excellence ensures your satidfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+
+      <div
+        className="flex-1 flex justify-centeric
+      "
+      >
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
